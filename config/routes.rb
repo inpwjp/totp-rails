1965b3d8c543175587efb0049882789bb82a521e
inpw <inpw@sv1.yokoyama-you.net>
2019-04-09 02:48:23 +0900

    reset 1st

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@
 Rails.application.routes.draw do
   match 'app_id', to: 'app_id#app_id', via: %i[get], as: 'app_id'  
   resources 'totp', only: %i[edit create destroy]
-  resources 'fido', only: %i[edit create destroy]
   resources :sessions, only: %i[new create destroy]
   resources :users
   match 'totp/check', to: 'totp#check', via: %i[get post], as: 'check_totp'
-  match 'fido/check', to: 'fido#check', via: %i[get post], as: 'check_fido'
 
   root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
